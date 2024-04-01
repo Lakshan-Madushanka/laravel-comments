6ca2133b33bfd280118a4c3e6a3111d479cce3cc
Lakshan Madushanka <epmadushanka@gmail.com>
2024-04-01 18:03:54 +0530

    Quill editor

--- config/comments.php
+++ config/comments.php
@@ -1,7 +1,5 @@
 <?php
 
-return [];
-use App\Models\User;
 use LakM\Comments\CommentPolicy;
 use LakM\Comments\Models\Comment;
 
@@ -19,3 +17,22 @@ return [
     'permissions' => [
         'create-comment' => [CommentPolicy::class, 'create'],
     ],
+
+    /**
+     * Quill editor configs
+     * @see https://quilljs.com/docs/configuration
+     */
+    'editor_config' => [
+        'debug' => 'info',
+        'modules' => [
+            'toolbar' => [
+                [['header' => [1, 2, false]]],
+                ['bold', 'italic', 'underline'],
+                ['code-block'],
+            ],
+        ],
+        'placeholder' => 'write your comment',
+        'theme' => 'snow',
+    ],
+
+];
---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import Quill from 'quill';
+window.Quill = Quill;
+
+import '../css/app.css';
+import 'quill/dist/quill.snow.css';
+
+
