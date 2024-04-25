27941a58d1ce395ea1411938ed7fbfee5ba4f9be
Lakshan Madushanka <epmadushanka@gmail.com>
2024-04-25 20:10:21 +0530

    Install and setup highlight.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,5 +4,10 @@ window.Quill = Quill;
 import moment from 'moment';
 window.moment = moment;
 
+import hljs from 'highlight.js';
+window.hljs = hljs;
+
 import '../css/app.css';
 import 'quill/dist/quill.snow.css';
+import 'highlight.js/styles/github.css';
+
