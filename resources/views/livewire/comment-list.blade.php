990ae542de45d190f05d4557b303706a0098a339
Lakshan Madushanka <epmadushanka@gmail.com>
2024-06-19 11:24:40 +0530

    Move comment to CommentItem component: This fixes related dom diffing issues

--- resources/views/livewire/comment-list.blade.php
+++ resources/views/livewire/comment-list.blade.php
@@ -9,6 +9,16 @@
             return '(' + this.total + ')'
         },
     }"
+    @comment-updated.window="(e) => {
+                        if(@js($model->approvalRequired())) {
+                            let elm = 'comment'+ key;
+                             setTimeout(() => {
+                               $refs[elm].remove();
+                               total -= 1;
+                             }, 2000);
+                            return;
+                        }
+                }"
     class="space-y-6"
 >
     <div class="text-lg font-bold">
@@ -71,166 +81,16 @@
 
     @if ($comments->isNotEmpty())
         @foreach ($comments as $comment)
-            <div x-ref="comment{{ $comment->getKey() }}" wire:key="comment-{{$loop->index}}" class="flex gap-x-2 sm:gap-x-4">
-                <div class="basis-14">
-                    <a href="{{ $profileUrl ?? $comment->ownerPhotoUrl($authMode) }}" target="_blank">
-                        <img
-                            class="h-12 w-12 rounded-full border border-gray-200"
-                            src="{{ $comment->ownerPhotoUrl($authMode) }}"
-                            alt="{{ $comment->ownerName($authMode) }}"
-                        />
-                    </a>
-                </div>
-                <div
-                    x-data="{ showUpdateForm: false }"
-                    @comment-update-discarded.window="(e) => {
-                                 if(e.detail.commentId === @js($comment->getKey())) {
-                                       showUpdateForm = false;
-                                 }
-                            }"
-                    class="basis-full"
-                >
-                    <div x-show="!showUpdateForm" x-transition class="rounded border border-gray-200">
-                        <div
-                            class="mb-2 flex flex-col items-start border-b border-gray-100 bg-gray-100 p-1 sm:flex-row sm:items-center sm:justify-between"
-                        >
-                            <div class="space-x-1">
-                                <span class="font-bold sm:hidden">
-                                    {{ Str::limit($guestMode ? $comment->guest_name : $comment->commenter->name, 10) }}
-                                </span>
 
-                                <span class="hidden font-bold sm:inline">
-                                    {{ Str::limit($guestMode ? $comment->guest_name : $comment->commenter->name, 25) }}
-                                </span>
+                <livewire:comments-item
+                    :key="'comment'. $comment->id"
+                    :$comment
+                    :$guestMode
+                    :$model
+                    :$showReplyList
+                    :$loop
+                />
 
-                                <span class="inline-block h-2 w-[1px] bg-black"></span>
-
-                                @if (config('comments.date_format') === 'diff')
-                                    <span class="text-xs">{{ $comment->created_at->diffForHumans() }}</span>
-                                @else
-                                    <span
-                                        x-text="moment(@js($comment->created_at)).format('YYYY/M/D H:mm')"
-                                        class="text-xs"
-                                    ></span>
-                                @endif
-
-                                @if ($comment->isEdited())
-                                    <span class="inline-block h-2 w-[1px] bg-black"></span>
-                                    <span class="text-xs">{{ __('Edited') }}</span>
-                                @endif
-                            </div>
-
-                            <div class="flex items-center justify-center space-x-2 sm:space-x-4">
-                                @if ($model->canEditComment($comment))
-                                    <div @click="showUpdateForm = !showUpdateForm" class="flex items-center">
-                                        <x-comments::action class="text-xs sm:text-sm">
-                                            {{ __('Edit') }}
-                                        </x-comments::action>
-                                    </div>
-                                @endif
-
-                                @if ($model->canDeleteComment($comment))
-                                    <div
-                                        wire:click="delete({{ $comment }})"
-                                        wire:confirm="{{ __('Are you sure you want to delete this comment?') }}"
-                                        class="flex items-center"
-                                    >
-                                        <x-comments::action
-                                            wire:loading.remove
-                                            wire:target="delete({{$comment}})"
-                                            class="align-text-bottom text-xs sm:text-sm"
-                                        >
-                                            {{ __('Delete') }}
-                                        </x-comments::action>
-                                        <x-comments::spin
-                                            wire:loading
-                                            wire:target="delete({{$comment}})"
-                                            class="!text-blue-500"
-                                        />
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                        <div
-                            x-ref="text"
-                            @comment-updated.window="(e) => {
-                                        let key = @js($comment->getKey());
-                                        if(e.detail.commentId === key) {
-                                            if(@js($model->approvalRequired())) {
-                                                let elm = 'comment'+ key;
-                                                 setTimeout(() => {
-                                                   $refs[elm].remove();
-                                                   total -= 1;
-                                                 }, 2000);
-                                                return;
-                                            }
-                                            $refs.text.innerHTML = e.detail.text;
-                                            showUpdateForm = false;
-                                        }
-                                    }"
-                            class="p-1"
-                        >
-                            {!! $comment->text !!}
-                        </div>
-                    </div>
-
-                    <!--Reaction manager -->
-                    <div x-show="!showUpdateForm" class="mt-2">
-                        <livewire:comments-reactions-manager :key="'reaction-manager-' . $loop->index" :$comment :relatedModel="$model" />
-                    </div>
-
-                    <!-- Update Form -->
-                    @if ($model->canEditComment($comment))
-                        <div x-show="showUpdateForm" x-transition class="basis-full">
-                            <livewire:comments-update-form :key="'update-form-'. $loop->index" :comment="$comment" :model="$model" />
-                        </div>
-                    @endif
-
-                    @if (config('comments.reply.enabled'))
-                        <div
-                            x-data="{ showReplyList: @js($showReplyList), replyCount: @js($comment->replies_count) }"
-                            @reply-created.window="
-                                    if($event.detail.commentId === {{ $comment->getKey() }}) {
-                                        replyCount += 1;
-                                    }
-                                  "
-                            @reply-deleted.window="
-                                    if($event.detail.commentId === {{ $comment->getKey() }}) {
-                                        replyCount -= 1;
-                                    }
-                                    "
-                            class="mt-2"
-                        >
-                            <div
-                                x-show="replyCount > 0"
-                                x-transition
-                                @click="$dispatch('show-replies.' + @js($comment->getKey())); showReplyList = !showReplyList"
-                                class="inline-block"
-                            >
-                                <x-comments::link
-                                    type="popup"
-                                    class="inline-flex items-center border-b-0 px-2 py-1 transition hover:rounded hover:border-b-0 hover:bg-gray-200 [&>*]:pr-1"
-                                >
-                                    <x-comments::icons.chevron-down x-show="!showReplyList" />
-                                    <x-comments::icons.chevron-up x-show="showReplyList" />
-                                    <span x-text="replyCount"></span>
-                                    <span>{{ __('replies') }}</span>
-                                </x-comments::link>
-                            </div>
-
-                            <!-- Reply List -->
-                            <div x-show="showReplyList" x-transtion class="ml-[-2rem] mt-6 sm:ml-8">
-                                <livewire:comments-reply-list
-                                    :key="'reply-list-'. $loop->index"
-                                    :$comment
-                                    :relatedModel="$model"
-                                    :total="$comment->replies_count"
-                                />
-                            </div>
-                        </div>
-                    @endif
-                </div>
-            </div>
         @endforeach
     @else
         <div class="text-lg">{{ __('Be the first one to make a comment !') }}</div>
---
+++ src/Livewire/CommentItem.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace LakM\Comments\Livewire;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Model;
+use LakM\Comments\Actions\DeleteCommentAction;
+use LakM\Comments\Actions\DeleteCommentReplyAction;
+use LakM\Comments\Models\Comment;
+use LakM\Comments\Models\Reply;
+use LakM\Comments\Repository;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\On;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class CommentItem extends Component
+{
+
+    #[Locked]
+    public Model $model;
+
+    #[Locked]
+    public Comment $comment;
+
+    #[Locked]
+    public bool $guestMode;
+
+    #[Locked]
+    public bool $authMode;
+
+    #[Locked]
+    public bool $showReplyList;
+
+    public function mount(
+        Comment $comment,
+        bool $guestMode,
+        Model $model,
+        bool $showReplyList,
+    ): void {
+        $this->comment = $comment;
+        $this->guestMode = $guestMode;
+        $this->model = $model;
+        $this->showReplyList = $showReplyList;
+        $this->authMode = !$guestMode;
+
+        $this->setProfileUrl();
+    }
+
+    public function delete(Comment $comment): void
+    {
+        if ($this->model->canDeleteComment($comment) && DeleteCommentAction::execute($comment)) {
+            $this->dispatch('comment-deleted', commentId: $comment->getKey());
+            $this->total -= 1;
+        }
+    }
+
+    private function setProfileUrl(): void
+    {
+        if ($user = $this->model->getAuthUser()) {
+            $this->profileUrl = $user->profileUrl();
+        }
+    }
+
+    public function render(): View|Factory|Application
+    {
+        return view('comments::livewire.comment-item');
+    }
+}
