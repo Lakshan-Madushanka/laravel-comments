a896d535098ff1b5eff8bb72f8853ce91e8b579b
Lakshan-Madushanka <epmadushanka@gmail.com>
2024-09-04 15:08:39 +0530

    Fix ability to exceed reply limit

--- src/Livewire/CreateCommentReplyForm.php
+++ src/Livewire/CreateCommentReplyForm.php
@@ -66,6 +66,10 @@ class CreateCommentReplyForm extends Component
     #[Locked]
     public bool $guestMode;
 
+    /**
+     * Current user reply count
+     * @var int
+     */
     public int $replyCount;
 
     public function boot(): void
@@ -183,10 +187,6 @@ class CreateCommentReplyForm extends Component
 
     public function incrementReplyCount(): void
     {
-        if ($this->approvalRequired) {
-            return;
-        }
-
         $this->replyCount += 1;
     }
 
