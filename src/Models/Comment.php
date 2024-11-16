aec98ca8b1a03914110cf192b02a3126626b0d0b
Lakshan Madushanka <epmadushanka@gmail.com>
2024-11-16 18:06:06 +0530

    Fix comments sort by top algorithm throws error for pgsql

--- src/Models/Comment.php
+++ src/Models/Comment.php
@@ -3,10 +3,13 @@
 namespace LakM\Comments\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\DB;
 use LakM\Comments\Builders\MessageBuilder;
+use LakM\Comments\ModelResolver;
 use LakM\Comments\ModelResolver as M;
 use LakM\Comments\Models\Concerns\HasOwner;
 use LakM\Comments\Models\Concerns\HasProfilePhoto;
@@ -50,5 +53,36 @@ class Comment extends Message
     public function replyReactions(): HasManyThrough
     {
         return $this->hasManyThrough(M::reactionClass(), Reply::class, 'reply_id', 'comment_id');
+    }
+
+    public function scopeAddScore(Builder $query): Builder
+    {
+        $reactionsTable = ModelResolver::reactionModel()->getTable();
+        $commentsTable = ModelResolver::commentModel()->getTable();
+
+        $reactionsCount = "(select count(*) from {$reactionsTable} where
+            {$commentsTable}.id = {$reactionsTable}.comment_id)";
+
+        $dislikesCountQuery = "(select (count(*) * 2) from {$reactionsTable} where
+            {$commentsTable}.id = {$reactionsTable}.comment_id and type = 'dislike')";
+
+        $repliesCountQuery = "(select (count(*) * 2) from {$commentsTable} as laravel_reserved_0 where
+            {$commentsTable}.id = laravel_reserved_0.reply_id)";
+
+        $replyReactionsCount = "(select count(*) from {$reactionsTable} inner join {$commentsTable} as laravel_reserved_1
+            on laravel_reserved_1.id = {$reactionsTable}.comment_id where {$commentsTable}.id = laravel_reserved_1.reply_id)";
+
+        $replyReactionsDislikeCount = "(select (count(*) * 2) from  {$reactionsTable}  inner join {$commentsTable} as laravel_reserved_2
+            on laravel_reserved_2.id = {$reactionsTable}.comment_id  where  {$commentsTable}.id = laravel_reserved_2.reply_id and
+            type = 'dislike')";
+
+        return $query->addSelect(DB::raw('(select ' .
+            $reactionsCount . ' + ' .
+            $repliesCountQuery . ' + ' .
+            $replyReactionsCount . ' - '.
+            $dislikesCountQuery . ' - ' .
+            $replyReactionsDislikeCount . ') ' .
+            'as score')
+        );
     }
 }
