f8c1d33a7854296360d1ed9775dd60f2567545e8
Lakshan Madushanka <epmadushanka@gmail.com>
2025-02-03 13:34:27 +0530

    fix: Root element styles are not applied.

--- resources/views/components/index.blade.php
+++ resources/views/components/index.blade.php
@@ -2,17 +2,20 @@
     'model',
 ])
 
-<div
-    x-cloak
-    x-data
-    class="space-y-8 dark:text-white lakm_commenter"
->
-    @if(config('comments.should_confirm_link_visit'))
-        <x-comments::link-visit-confirm-modal/>
-    @endif
-    <livewire:comments-list :model="$model"/>
-    <hr class="text-gray-400"/>
-    <div id="create-comment-form">
-        <livewire:comments-create-form :model="$model"/>
+<div class="lakm_commenter">
+    <div
+        x-cloak
+        x-data
+        class="space-y-8 dark:text-white"
+    >
+        @if(config('comments.should_confirm_link_visit'))
+            <x-comments::link-visit-confirm-modal/>
+        @endif
+        <livewire:comments-list :model="$model"/>
+        <hr class="text-gray-400"/>
+        <div id="create-comment-form">
+            <livewire:comments-create-form :model="$model"/>
+        </div>
     </div>
 </div>
+
