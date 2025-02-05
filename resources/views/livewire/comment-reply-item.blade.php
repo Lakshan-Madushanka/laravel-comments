85e18c1e962701ba9b687b1362b42cf85edd58e0
zvizvi <5633910@gmail.com>
2025-02-05 14:09:49 +0200

    Correct time format

--- resources/views/livewire/comment-reply-item.blade.php
+++ resources/views/livewire/comment-reply-item.blade.php
@@ -77,7 +77,7 @@
                         <span class="inline-block h-2 w-[1px] bg-black mr-1"></span>
 
                         @if (config('comments.date_format') === 'diff')
-                            <span class="text-xs" x-bind:title="moment(@js($reply->created_at)).format('YYYY-MM-DD HH:mm')">{{ $reply->created_at->diffForHumans() }}</span>
+                            <span class="text-xs" x-bind:title="moment(@js($reply->created_at)).format('YYYY/M/D H:mm')">{{ $reply->created_at->diffForHumans() }}</span>
                         @else
                             <span
                                 x-text="moment(@js($reply->created_at)).format('YYYY/M/D H:mm')"
