2613362af9f078e8c3600f735f8f1ffa098fe41e
Lakshan Madushanka <epmadushanka@gmail.com>
2026-02-26 17:12:35 +0530

    Fix: Vite manifest generating absolute paths (#188)

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    root: __dirname, // 👈 FORCE correct root
+    base: '',
     plugins: [
         laravel({
             hotFile: 'public/commenter.hot', // Most important lines
-            buildDirectory: '/build', // Most important lines
+            buildDirectory: 'build', // Most important lines
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
