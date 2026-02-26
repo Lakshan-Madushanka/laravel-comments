f023dd0ed2c79e8ca8c6bc8ecf497ded97a7f098
Lakshan Madushanka <epmadushanka@gmail.com>
2026-02-26 16:59:44 +0530

    Fix: Not showing single thread (#184)

--- src/Livewire/Concerns/HasSingleThread.php
+++ src/Livewire/Concerns/HasSingleThread.php
@@ -2,65 +2,33 @@
 
 namespace LakM\Commenter\Livewire\Concerns;
 
-use Illuminate\Support\Str;
-use LakM\Commenter\Helpers;
+use Livewire\Attributes\Url;
+use Livewire\Livewire;
 
 trait HasSingleThread
 {
+    #[Url]
+    public string $message_type = '';
+
+    #[Url]
+    public string $message_id = '';
+
     public function shouldShowSingleThread(): bool
     {
-        $query = parse_url(Helpers::livewireCurrentURL(), PHP_URL_QUERY);
-
-        if (empty($query)) {
-            return false;
+        if ($this->message_id && $this->message_type === 'single') {
+            return true;
         }
 
-        parse_str($query, $query);
-
-        $query = collect($query);
-
-        if (!($query->has('message_type') && $query->get('message_type') === 'single')) {
-            return false;
-        }
-
-        if (!($query->has('message_id') && filled($query->get('message_id')))) {
-            return false;
-        }
-
-        return true;
+        return false;
     }
 
-    public function showFullThread()
+    public function showFullThread(): void
     {
-        $currentURL = Helpers::livewireCurrentURL();
-
-        $query = parse_url($currentURL, PHP_URL_QUERY);
-
-        if (empty($query)) {
-            return false;
-        }
-
-        parse_str($query, $query);
-
-        $query = collect($query);
-
-        $query->forget(['message_type', 'message_id']);
-
-        $currentURL = Str::before($currentURL, '?');
-
-        if ($query->isNotEmpty()) {
-            $currentURL .= '?' . http_build_query($query->toArray());
-        }
-
-        $this->redirect($currentURL);
+        $this->redirect(Livewire::originalUrl());
     }
 
     public function referencedCommentId(): string
     {
-        $query = parse_url(Helpers::livewireCurrentURL())['query'];
-
-        parse_str($query, $query);
-
-        return collect($query)->get('message_id');
+        return $this->message_id;
     }
 }
